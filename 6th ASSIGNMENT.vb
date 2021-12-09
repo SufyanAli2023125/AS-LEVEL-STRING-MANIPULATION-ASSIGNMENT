86d4a1de3f921eca6c0c13093d93530b61f8dcd9
SufyanAli2023125 <93531780+SufyanAli2023125@users.noreply.github.com>
2021-12-09 22:36:05 +0500

    Add files via upload

---
+++ 6th ASSIGNMENT.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim i, donutcounter, donuts As Integer
+
+        donuts = 0
+        donutcounter = 0
+        i = 0
+
+        Console.WriteLine("donuts: ")
+        donutcounter = Console.ReadLine
+
+        For i = 1 To 10
+            If donutcounter >= "1" And donutcounter <= "10" Then
+                str1 = donutcounter
+
+                Console.WriteLine("number of donuts: " & str1)
+
+
+            ElseIf donutcounter > 10 Then
+                Console.WriteLine("too many donuts")
+            End If
+            Console.ReadKey()
+        Next
+    End Sub
+
+End Module
