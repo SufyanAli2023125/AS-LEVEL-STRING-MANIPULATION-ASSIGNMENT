3d80703151f568f8099122fc60513c48086485c0
SufyanAli2023125 <93531780+SufyanAli2023125@users.noreply.github.com>
2021-12-09 22:36:23 +0500

    Add files via upload

---
+++ 7th ASSIGNMENT.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s, str2, str3 As String
+        Dim char1 As Char
+        Dim i As Integer
+
+        s = ""
+        str2 = ""
+        str3 = ""
+        char1 = ""
+        i = 0
+
+        Console.WriteLine("enter your string: ")
+        s = Console.ReadLine
+
+        str2 = Left(s, 2)
+        str3 = Right(s, 2)
+
+        For i = 1 To Len(s)
+            char1 = Mid(s, 3, 2)
+
+            str2 = str2 & str3
+
+
+
+            Console.WriteLine("altered string: " & str2)
+            Console.ReadKey()
+
+        Next
+
+
+
+    End Sub
+
+End Module
