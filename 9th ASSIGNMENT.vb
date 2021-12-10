361da3852947d14f5073b353da8471b603a600cb
SufyanAli2023125 <93531780+SufyanAli2023125@users.noreply.github.com>
2021-12-10 18:50:07 +0500

    Add files via upload

---
+++ 9th ASSIGNMENT.vb
@@ -0,0 +1,67 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, str1, str2, e, f, g, h, l As String
+        Dim i, j, z As Integer
+
+        a = ""
+        b = ""
+        str1 = ""
+        str2 = ""
+        e = ""
+        f = ""
+        g = ""
+        h = ""
+        l = ""
+        z = 0
+        i = 0
+        j = 0
+
+        Console.WriteLine("enter first string: ")
+        str1 = Console.ReadLine
+
+        Console.WriteLine("enter second string: ")
+        str2 = Console.ReadLine
+
+        For i = 1 To Len(str1)
+
+            e = Mid(str1, 1, 2)
+            a = e
+
+        Next
+
+        For j = 1 To Len(str2)
+            f = Mid(str2, 1, 2)
+            b = f
+
+        Next
+
+        For i = 1 To Len(str1)
+            g = Right(str1, 1)
+
+
+        Next
+
+        For j = 1 To Len(str2)
+            h = Right(str2, 1)
+
+        Next
+
+
+        For z = 1 To Len(str2)
+            l = Mid(str2, 3)
+
+
+        Next
+
+
+
+        Console.WriteLine("your altered string: " & b & g & " " & a & l)
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
